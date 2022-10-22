a1b51d8f90fca2ac967c9364556d94151ced03f9
Nouran kadri <101168150+engnouran12@users.noreply.github.com>
2022-10-22 08:05:51 +0200

    bb

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/weather_provider.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+     create: (context) {
+        return WeatherProvider();
+      },
+    child: WeatherApp()));
+}
+
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+      primarySwatch: Provider.of<WeatherProvider>(context).weatherData == null ?  Colors.blue : Provider.of<WeatherProvider>(context).weatherData!.getThemeColor()  ,
+      ),
+      home: HomePage(),
+
+    );
+  }
+}
